a32926950a6053bb156bfb420174ea9e50e6e084
yagneshdhanani <yagneshdhanani791@gmail.com>
2021-12-02 12:06:04 +0530

    final

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,43 +3,22 @@ import PropTypes from "prop-types";
 
 // import { Link } from "react-router-dom";
 import CustomBadge from "./CustomBadge";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 export default function NavBar({ title, mode, setMode, clrObj }) {
-  const variant = mode === "info" || mode === "light" ? "light" : "dark";
+  // const variant = mode === "info" || mode === "light" ? "light" : "dark";
 
   return (
-    <nav
-      className={`navbar navbar-expand-lg navbar-${variant}`}
-      style={{ backgroundColor: `${clrObj.backgroundColor}` }}
-    >
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          {title}
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                About
-              </a>
-            </li>
-          </ul>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#">{title}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">About</Nav.Link>
+          </Nav>
+
           <CustomBadge type="primary" setMode={setMode} txt="Blue" />
           <CustomBadge type="secondary" setMode={setMode} txt="Gray" />
           <CustomBadge type="success" setMode={setMode} txt="Green" />
@@ -53,9 +32,9 @@ export default function NavBar({ title, mode, setMode, clrObj }) {
             textType="text-dark"
           />
           <CustomBadge type="dark" setMode={setMode} txt="Dark" />
-        </div>
-      </div>
-    </nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
