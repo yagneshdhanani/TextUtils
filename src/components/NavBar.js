85164ff45b4bc5a43557264ac6a7715c1763ca56
yagneshdhanani <yagneshdhanani791@gmail.com>
2021-12-02 11:41:19 +0530

    final

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,7 +13,7 @@ export default function NavBar({ title, mode, setMode, clrObj }) {
       style={{ backgroundColor: `${clrObj.backgroundColor}` }}
     >
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" href="#">
           {title}
         </a>
         <button
@@ -30,12 +30,12 @@ export default function NavBar({ title, mode, setMode, clrObj }) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <a className="nav-link active" aria-current="page" href="#">
                 Home
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <a className="nav-link" href="#">
                 About
               </a>
             </li>
